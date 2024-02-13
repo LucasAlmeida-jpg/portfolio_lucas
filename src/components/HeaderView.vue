0bf8413e799d8a0bf9ddc55c0f5d65aa7b616644
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2024-02-12 22:25:47 -0300

    fix the bug

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,25 +1,55 @@
-
-
 <template>
     <nav class="d-flex align-items-center justify-content-between py-3 px-5">
-        <div class="d-flex align-items-center">
-            <h1 class="wave-text">Lucas Almeida</h1>
-        </div>
-        <div>
-            <ul class="d-flex gap-5 list-style-none">
-                <li class="list-group-item">{{ $t('aboutMe') }}</li>
-                <li class="list-group-item">{{ $t('habilities') }}</li>
-                <li class="list-group-item">{{ $t('contact') }}</li>
-                <li class="list-group-item"><a href="https://github.com/LucasAlmeida-jpg" target="_blank"
-                        rel="noopener noreferrer">{{ $t('github') }}</a></li>
-                <li class="list-group-item">
-                    <button @click="$i18n.locale = `EN`">EN</button>
-                    <button @click="$i18n.locale = `PT`">PT</button>
-                </li>
-            </ul>
-        </div>
+      <div class="d-flex align-items-center">
+        <h1 class="wave-text"><span>Lucas Almeida</span></h1>
+      </div>
+      <div>
+        <ul class="d-flex gap-5 list-style-none">
+          <li class="list-group-item">{{ $t('aboutMe') }}</li>
+          <li class="list-group-item">{{ $t('habilities') }}</li>
+          <li class="list-group-item">{{ $t('contact') }}</li>
+          <li class="list-group-item">
+            <a href="https://github.com/LucasAlmeida-jpg" target="_blank" rel="noopener noreferrer">{{ $t('github') }}</a>
+          </li>
+          <li class="list-group-item language-switch d-flex align-items-center">
+            <button @click="$i18n.locale = 'EN'" :class="{ active: $i18n.locale === 'EN' }">EN(US)</button>
+            <button @click="$i18n.locale = 'PT'" :class="{ active: $i18n.locale === 'PT' }">PT(BR)</button>
+          </li>
+        </ul>
+      </div>
     </nav>
-</template>
+  </template>
+  
+  <style scoped>
+ 
+  .language-switch button {
+    border: none;
+    background: none;
+    cursor: pointer;
+    font-size: 16px;
+    padding: 5px;
+  }
+  
+  .language-switch button {
+  border: none;
+  background: none;
+  cursor: pointer;
+  font-size: 13px;
+  margin-top: -5px;
+  color: white;
+  transition: color 0.3s ease-in-out;
+}
+
+.language-switch button:hover {
+  color: var( --vt-bg-primary); 
+}
+
+.language-switch button.active {
+  font-weight: bold;
+  color: var( --vt-bg-primary); 
+}
+  </style>
+  
   
 <style>
 @keyframes wave {
@@ -36,7 +66,7 @@
 
 .wave-text {
     font-size: 2em;
-    animation: wave 1.5s infinite;
+    /* animation: wave 1.5s infinite; */
     color: rgb(230, 219, 219);
     font-weight: bold;
 }

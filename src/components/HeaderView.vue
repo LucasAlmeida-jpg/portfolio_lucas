f997005ae7ef0d4c64aa839b2cc7c4e63cb2258b
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2024-03-27 00:22:29 -0300

    akjhajkshd

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar nav navbar-expand-lg py-3">
     <div class="container-fluid">
-      <a class="navbar-brand animate__animated animate__flipInY" href="#" @click="closeMobileMenu()">Lucas Almeida</a>
+      <a class="navbar-brand animate__animated animate__rotateInDownLeft" href="#" @click="closeMobileMenu()">Lucas Almeida</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>

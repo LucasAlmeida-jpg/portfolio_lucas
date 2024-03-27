1232a2089fc7b5f3850d536734e40d7eb8270544
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2024-03-27 00:42:06 -0300

    akjhajkshd

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,5 +1,5 @@
 <template>
-  <section  class="slide-in">
+  <section class="slide-in">
     <div class="text-center">
       <h1 class="animate__animated animate__flipInY">
         <span>{{ $t('developer') }}</span> {{ $t('frontEnd') }} <span>{{ $t('vue') }}</span>
@@ -18,31 +18,36 @@
     <div class="container">
       <ul class="nav nav-pills my-3 text-center d-flex justify-content-center" id="pills-tab" role="tablist">
         <li class="nav-item" role="presentation">
-          <div class="nav-link active animate__animated animate__flipInY" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home"
-            role="tab" aria-controls="pills-home" aria-selected="true">{{ $t('professionals_tag') }}</div>
+          <div class="nav-link active animate__animated animate__flipInY" id="pills-home-tab" data-bs-toggle="pill"
+            data-bs-target="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">{{
+          $t('professionals_tag') }}</div>
         </li>
         <li class="nav-item" role="presentation">
-          <div class="nav-link animate__animated animate__flipInY" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" role="tab"
-            aria-controls="pills-profile" aria-selected="false">{{ $t('educational_tag') }}</div>
+          <div class="nav-link animate__animated animate__flipInY" id="pills-profile-tab" data-bs-toggle="pill"
+            data-bs-target="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">{{
+          $t('educational_tag') }}</div>
         </li>
       </ul>
       <div class="tab-content" id="pills-tabContent">
 
         <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab"
-          tabindex="0">      
+          tabindex="0">
           <div class="row my-3 d-flex align-items-center justify-content-center">
             <div class="col-md-6 text-center animate__animated animate__flipInY">
               <div>
                 <h1>{{ $t('experience_tag') }}</h1>
               </div>
-              <img class="mb-2 animate__animated animate__rotateIn" src="../assets/images/Group.svg" alt="">
+              <a href="https://creators.llc/" target="_blank">
+                <img class="mb-2 animate__animated animate__rotateIn" src="../assets/images/Group.svg" alt="">
+              </a>
               <p><b>Creators LLC</b> - <span>{{ $t('developer') }} {{ $t('frontEnd') }}</span> <b>(2022 - {{
           $t('currently') }})</b></p>
               <div class="d-flex align-items-center">
                 <p>{{ $t('experience') }}</p>
               </div>
             </div>
-            <div class="col-md-6 presentations animate__animated animate__flipInY"><img class="img-presentation" src="../assets/images/computer.svg" alt=""></div>
+            <div class="col-md-6 presentations animate__animated animate__flipInY"><img class="img-presentation"
+                src="../assets/images/computer.svg" alt=""></div>
           </div>
 
         </div>
@@ -54,7 +59,8 @@
               <p>{{ $t('institute') }}</p>
               <img class="if-institute" src="../assets/if.png" alt="">
             </div>
-            <div class="col-md-6 presentations animate__animated animate__flipInY"><img class="img-presentation" src="../assets/images/html.svg" alt=""></div>
+            <div class="col-md-6 presentations animate__animated animate__flipInY"><img class="img-presentation"
+                src="../assets/images/html.svg" alt=""></div>
           </div>
 
           <div class="row my-3 d-flex align-items-center justify-content-center animate__animated  animate__flipInY">
@@ -73,7 +79,7 @@
 
   <section class="skills">
     <div class="text-center">
-      <h1 > SKILLS</h1>
+      <h1> SKILLS</h1>
 
       <div class="row d-flex justify-content-center">
         <p>Front-End</p>
@@ -102,14 +108,17 @@
         <p>Mande uma mensagem</p>
         <div class="col-md-12">
           <p class="mt-2 text-center">
-            Entre em contato através dos dados ou enviando um e-mail para<a href="mailto:lucasalmeidagouvea123@gmail.com">este e-mail.</a>
+            Entre em contato através dos dados ou enviando um e-mail para<a
+              href="mailto:lucasalmeidagouvea123@gmail.com">este e-mail.</a>
           </p>
         </div>
 
         <div class="d-flex align-items-center justify-content-center social">
           <div>
-            <a class="me-3" href="http://" target="_blank" rel="noopener noreferrer"><img src="/src/assets/images/github.png" alt=""></a>          
-            <a href="https://github.com/LucasAlmeida-jpg" target="_blank" rel="noopener noreferrer"><img src="/src/assets/images/linkedin.png" alt=""></a>
+            <a class="me-3" href="http://" target="_blank" rel="noopener noreferrer"><img
+                src="/src/assets/images/github.png" alt=""></a>
+            <a href="https://github.com/LucasAlmeida-jpg" target="_blank" rel="noopener noreferrer"><img
+                src="/src/assets/images/linkedin.png" alt=""></a>
           </div>
         </div>
       </div>
@@ -127,7 +136,7 @@ export default {
   },
 
   methods: {
-   
+
     //
   }
 }
@@ -150,6 +159,7 @@ section {
 
 
 }
+
 .tag:hover {
   box-shadow: 0 0 20px 1px rgba(0, 220, 130, 0.9);
 }
@@ -201,9 +211,11 @@ p {
   padding: 10px;
   height: 40px;
 }
+
 .social img {
   width: 30px;
 }
+
 @media only screen and (max-width: 600px) {
   .carousel-item {
     width: 107% !important;
@@ -219,18 +231,18 @@ p {
     justify-content: center;
   }
 
-  .skills{
+  .skills {
     padding: 0px 40px;
   }
 
-  .presentations{
+  .presentations {
     display: flex;
     justify-content: center;
   }
 
-  .presentation{
+  .presentation {
     display: none;
   }
-  
+
 }
 </style>

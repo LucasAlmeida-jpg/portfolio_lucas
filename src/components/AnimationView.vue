c2597eb7b4bc8937322aec84cad5be2005a7eb7e
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2024-04-07 22:13:08 -0300

    animation

--- src/components/AnimationView.vue
+++ src/components/AnimationView.vue
@@ -7,34 +7,16 @@
 </template>
 
 <style scoped>
-body {
-    margin: auto;
-    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-    overflow: auto;
-    background: linear-gradient(315deg, rgba(101,0,94,1) 3%, rgba(60,132,206,1) 38%, rgba(48,238,226,1) 68%, rgba(255,25,25,1) 98%);
-    animation: gradient 15s ease infinite;
-    background-size: 400% 400%;
-    background-attachment: fixed;
-}
 
-@keyframes gradient {
-    0% {
-        background-position: 0% 0%;
-    }
-    50% {
-        background-position: 100% 100%;
-    }
-    100% {
-        background-position: 0% 0%;
-    }
-}
+
+
 
 .wave {
-    background: var(--vt-bg-primary);
+    background: rgba(40, 61, 59, 140);
     border-radius: 1000% 1000% 0 0;
     position: fixed;
     width: 200%;
-    height: 50%;
+    height: 70%;
     animation: wave 10s -3s linear infinite;
     transform: translate3d(0, 0, 0);
     opacity: 0.2;
@@ -45,7 +27,7 @@ body {
 
 .wave:nth-of-type(2) {
     bottom: -1.25em;
-    animation: wave 18s linear reverse infinite;
+    animation: wave 78s linear reverse infinite;
     opacity: 0.2;
 }
 

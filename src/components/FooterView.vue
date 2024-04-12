3f722e23d6783c4dd6401c3ffe72fbcc16e6d664
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2024-04-12 11:04:06 -0300

    adjust

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,16 +1,25 @@
 <template>
     <footer class="footer-bg" id="footer">
+       <div>
+        <span>Codificado por mim. Especialmente para praticar <img class="vue" src="../assets/logo.svg" alt=""></span>
+       </div>
+       <div>
         <span>© 2024 Lucas Gouvea de Almeida </span>   
+       </div>
     </footer>
 </template>
 
 <style scoped>
 footer{
-    display: flex;
+    display: block;
     align-items: center;
     justify-content: center;
     width: 100% !important;
     bottom: 0 !important;
+}
+
+.vue{
+    width: 20px;
 }
 
 </style>
